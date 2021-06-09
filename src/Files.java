aca845719dd6a7fb7725f707c481986f5bc8e8c1
julnow <jul.nowk@gmail.com>
2021-06-09 12:09:03 +0200

    new files class (to omit problems with the same name)

---
+++ src/Files.java
@@ -0,0 +1,20 @@
+
+public class Files {
+	
+	String sha512;
+	String address;
+	
+	Files(String sha512, String address){
+		this.sha512 = sha512;
+		this.address = address;
+	}
+	
+	String getAddress() {
+		return address;
+	}
+	
+	String getSha() {
+		return sha512;
+	}
+
+}
